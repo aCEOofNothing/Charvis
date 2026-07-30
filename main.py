94fdc5a6e1406bf3247503c1e48ba0a4cc1c1681
Mael Schanjar <mael.schanjar@outlook.de>
2026-07-30 23:13:46 +0200

    bug fixes. jahahahuiy!

--- main.py
+++ main.py
@@ -24,26 +24,43 @@ print("♾️    Charvis Ist Bereit!", end="\n\n")
 
 def process_input(text):
     global mode
+
+    BASE_DIR = Path(__file__).parent
+    SETTINGS = BASE_DIR / "data" / "settings.json"
+    with open(SETTINGS, "r") as file:
+        settings = json.load(file)
+    last_mode = mode
+    mode = settings["triggerkey"]
+
     if "öffne die oberfläche" in text:
         subprocess.Popen(["Python", "gui/flaskgui.py"])
         webbrowser.open("http://127.0.0.1:5000", new=1)
 
     if "eingabe" in text:
         if "sprache" in text:
-                mode = "speech"
-                print("Zu Spracheingabe gewechselt")
-                print("Halte NUMPAD-0 zum Sprechen...")
-                settings["eingabemodus"] = "speech"
-                with open(SETTINGS, "w") as file:
-                    json.dump(settings, file, indent=4)
+            mode = "speech"
+            print("Zu Spracheingabe gewechselt")
+            print("Halte NUMPAD-0 zum Sprechen...")
+            settings["eingabemodus"] = "speech"
+            with open(SETTINGS, "w") as file:
+                json.dump(settings, file, indent=4)
         elif "terminal" in text:
-                mode = "terminal"
-                print("Zu Terminaleingabe gewechselt")
-                settings["eingabemodus"] = "terminal"
-                with open(SETTINGS, "w") as file:
-                    json.dump(settings, file, indent=4)
+            mode = "terminal"
+            print("Zu Terminaleingabe gewechselt")
+            settings["eingabemodus"] = "terminal"
+            with open(SETTINGS, "w") as file:
+                json.dump(settings, file, indent=4)
         else:
-                print("❌ Bitte gib an, welchen Eingabemodus du öffnen möchtest ('Sprache' oder 'Terminal')")
+            print("❌ Bitte gib an, welchen Eingabemodus du öffnen möchtest ('Sprache' oder 'Terminal')")
+
+    elif last_mode != mode:
+        if mode == "speech":
+            print("Zu Spracheingabe gewechselt")
+            print("Halte NUMPAD-0 zum Sprechen...")
+        elif mode == "terminal":
+            print("Zu Terminaleingabe gewechselt")
+        else:
+            print("❌ Bitte gib an, welchen Eingabemodus du öffnen möchtest ('Sprache' oder 'Terminal')")
 
     else:
         core.handle_command(text)
